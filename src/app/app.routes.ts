447ca15167a731630aec138c44c78744af37256c
GeraldineBecerra <gera.becerra@duocuc.cl>
2026-04-23 12:56:30 -0400

    configuracion inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { UserForm } from './features/user/user-form/user-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+
+            // {
+            //     path: 'dashboard',
+            //     loadComponent: () =>
+            //         import('./pages/dashboard/dashboard.component')
+            //             .then(m => m.DashboardComponent)
+            // },
+
+            {
+                path: 'users',
+                loadComponent: () =>
+                    import('./features/user/user-list/user-list')
+                        .then(m => m.UserList)
+            },
+            {
+                path: 'users/new',
+                component: UserForm
+            },
+
+            {
+                path: 'companies',
+                loadComponent: () =>
+                    import('./features/company/company-list/company-list')
+                        .then(m => m.CompaniesList)
+            },
+            {
+                path: 'companies/new',
+                loadComponent: () =>
+                    import('./features/company/company-form/company-form')
+                        .then(m => m.CompanyForm)
+            }
+
+        ]
+    }
+
+];
